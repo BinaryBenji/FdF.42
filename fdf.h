d8e60f6ce68cb64abd3464606088ab6ba672a828
Benjamin <ben639dx@gmail.com>
2017-02-14 15:31:25 +0100

    more

---
+++ fdf.h
@@ -0,0 +1,15 @@
+#ifndef FDF_H
+# define FDF_H
+
+	typedef struct s_coord
+	{
+		int x;
+		int y;
+		int index;
+	}			   t_coord;
+
+	
+
+
+
+#endif
--- main.c
+++ main.c
@@ -1,4 +1,14 @@
 #include "mlx.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void 	draw_line() // 
+{
+
+
+}
+
+t_coord		*assign_coord()
 
 int main()
 {
@@ -6,26 +16,56 @@ int main()
 	void *win;
 	int x;
 	int y;
-
+	int i = 0;
+	char donnees[20] = "01111111111111111110";
+	int turn = 0;
+	// 
+	// algo de bresenham pour tracer une ligne
+	// calcul du centre a partir de la taille d'une ligne du fichier
+	y = 200;
+	x = 200;
 	mlx = mlx_init();
-	win = mlx_new_window(mlx, 500, 500, "MLX");
-	
+	win = mlx_new_window(mlx, 1000, 1000, "MLX");
+
+	while (donnees[i])
+	{	
+		if (donnees[i] == '0')
+		{	
+			mlx_pixel_put(mlx,win,x,y,0x00FFFFFF);
+		}
+		else if(donnees[i] == '1')
+		{
+			x+=5;
+			x+=5;
+			mlx_pixel_put(mlx,win,x,y,0x00FFFFFF);
+		}
+		// else
+		// {
+		// 	exit(0);
+		// }
+		x+=20;
+		y+=20;
+		turn++;
+		if(turn == 5)
+		{
+			x=220;
+
+			turn = 0;
+		}
+		i++;
+	}
+		//x+=20;
+		//y +=20;
 	// no forget exit();
-	y = 0;
-	x = 0;
-	while (x < 500 && y < 500)
-	{
-		mlx_pixel_put(mlx, win, x, y,0x00FFFFFF);
-		x++;
-		y++;
-	}
-	x = 500;
-	y = 250;
-	while (x > 0 && y > 0)
-	{
-		mlx_pixel_put(mlx, win, x, y,0x00FFFFFF);
-		x--;
-		y--;
-	}
+
+	// while (x < 500 && y < 500)
+	// {
+	// 	mlx_pixel_put(mlx, win, x++, y,0x00FFFFFF);
+	// 	if (x == 500)
+	// 	{
+	// 		x = 0;
+	// 		y++;
+	// 	}
+	// }	
 	mlx_loop(mlx);
 }
